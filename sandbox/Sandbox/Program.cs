93b6fe357eb3dab2f80d7a19cd12fc8495b4d836
chloei04 <bsantos@byui.edu>
2025-01-10 15:58:52 +0800

    Finished exercise 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("This is in C#.");
+
+        int number = 5;
+        number = 8;
+        number = number + 3;
+        
+        Console.Write("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your color is {color}");
     }
 }
