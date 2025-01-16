01a64f0296104804689e15bdcea7dae6d61a547e
chloei04 <bsantos@byui.edu>
2025-01-16 23:07:18 +0800

    Finished Resume

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -11,8 +11,8 @@ class Program
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last}, {first} {last}");
-        
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
 
+        //Display the name
     }
 }
